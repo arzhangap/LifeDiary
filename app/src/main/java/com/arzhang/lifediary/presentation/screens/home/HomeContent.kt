2c0a5722793243c3a704e9d841424c593fb12ecf
arjang_ap <arjang.ap@gmail.com>
2024-04-11 12:40:47 +0330

    make diary header.

---
+++ app/src/main/java/com/arzhang/lifediary/presentation/screens/home/HomeContent.kt
@@ -0,0 +1,62 @@
+package com.arzhang.lifediary.presentation.screens.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.time.LocalDate
+
+@Composable
+fun DateHolder(localDate: LocalDate) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Column(horizontalAlignment = Alignment.End) {
+            // use desugar to bypass sdk requirements.
+            Text(
+                // values from 1 to 9 will have a zero before them. example: 09 but 10 will remain the same
+                text = String.format("%02d",localDate.dayOfMonth),
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontWeight = FontWeight.Light
+                )
+            )
+            Text(text = localDate.dayOfWeek.toString().take(3),
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                    fontWeight = FontWeight.Light
+                )
+            )
+        }
+        Spacer(Modifier.width(14.dp))
+        Column(horizontalAlignment = Alignment.Start) {
+            Text(
+                text = localDate.month.toString().lowercase().replaceFirstChar { it.titlecase() },
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontWeight = FontWeight.Light
+                )
+            )
+            Text(text = localDate.year.toString(),
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
+                style = TextStyle(
+                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                    fontWeight = FontWeight.Light
+                )
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true,)
+fun DateHeaderPreview() {
+    DateHolder(localDate = LocalDate.now())
+}
